afdfb7e62ec5705ef17affbfd34aaad2fc1d0403
root <root@Debian9SP.altoros.corp>
2018-08-14 17:26:25 +0300

    add color output

--- go_port.sh
+++ go_port.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
+# color
+c_def_back = "\e[44m"
+c_show_back = "\e[49m"
 # get random number for DPORT
 rdport=$(python -S -c "import random; print random.randrange(49152,65535)")
 
 # name of container
 dock_cont="npme-couch"
+#dock_cont="npme-rr-service"
 
 # get IP container
 ip=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' $dock_cont)
 # more long var for get IP container
-#ip=$(/usr/bin/docker inspect -f '{{.Name}} - {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq) | grep $dock_cont | awk '{print $3}')
+ip=$(/usr/bin/docker inspect -f '{{.Name}} - {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq) | grep $dock_cont | awk '{print $3}')
 
 # add NAT to iptables for access
-/sbin/iptables -t nat -A DOCKER -p tcp --dport $rdport -j DNAT --to-destination $ip:5984
+#/sbin/iptables -t nat -A DOCKER -p tcp --dport $rdport -j DNAT --to-destination $ip:5984
 
-echo "Container $dock_cont has IP= $ip"
-echo "Try to go http://NPME.DOMAIN:$rdport/registry/_all_docs"
+echo -e "\e[49m Container \e[44m $dock_cont \e[49m has IP=\e[44m $ip \e[49m"
+echo -e "\e[49m Try to go http://NPME.DOMAIN:\e[44m$rdport\e[49m/registry/_all_docs\e[49m"
 
