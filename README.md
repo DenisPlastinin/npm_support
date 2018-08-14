cd2ea0f425c57f48e9f133c1572a8e84247c78ea
root <root@Debian9SP.altoros.corp>
2018-08-14 13:16:13 +0300

    edit readme.md

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # npm_support
 some scripts for npme
+1. #./go_port.sh  
+	search cotainer "npme-couch"
+	get container IP
+	get random port between 49152 - 65535
+	add rule NAT to iptable :random_port -> ip_npme-couch:5984
+You may access http://NPME.IP:ramdom_port/registry/_all_docs
--- go_port.sh
+++ go_port.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # get random number for DPORT
 rdport=$(python -S -c "import random; print random.randrange(49152,65535)")
-# name of kontainer
-dock_kont="npme-couch"
-# get IP kontainer
-ip=$(/usr/bin/docker inspect -f '{{.Name}} - {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq) | grep $dock_kont | awk '{print $3}')
+# name of container
+dock_cont="npme-couch"
+# get IP container
+ip=$(/usr/bin/docker inspect -f '{{.Name}} - {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq) | grep $dock_cont | awk '{print $3}')
 # add NAT to iptables for access
 /sbin/iptables -t nat -A DOCKER -p tcp --dport $rdport -j DNAT --to-destination $ip:5984
-echo "Kontainer $dock_kont has IP= $ip"
+echo "Kontainer $dock_cont has IP= $ip"
 echo "Try to go http://NPME.DOMAIN:$rdport/registry/_all_docs"
 
